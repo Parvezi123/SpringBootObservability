f24d240cfd27bd61cb46108cd0137c0f79f770dd
Parvez Alam <parvezimohammed@gmail.com>
2023-04-06 07:44:38 +0530

    Created Base Project - Updated Pom.xml

---
+++ SpringBootObservability/src/test/java/parvez/springbootobservability/SpringBootObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package parvez.springbootobservability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootObservabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
